47a82a0e24c63989ebfbd5f4c24a5bbb10a116d1
Andrzej <andrzejtrg21@gmail.com>
2021-05-08 18:57:57 +0200

    labo z 08 V 2021 pętla for

---
+++ labo3/silnia for.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  8 10:46:34 2021
+
+@author: andrz
+"""
+"""
+n=3
+
+silnia = 1
+i = 1
+
+while i <= n:
+        silnia = silnia * i
+        print ('silnia = ', silnia)
+        
+        i = i + 1
+        print ('i=', i)
+print ('silnia = ', silnia)
+
+"""
+
+"""
+for k in range(10):
+    print("wynik", k)
+  
+ 
+
+silnia=1
+i=1
+n=4
+
+for i in range (1,n+1):
+    
+    silnia=silnia*i
+   
+print(silnia)
+        
+""" 
+
+
+
+
+
+
+
+
+
